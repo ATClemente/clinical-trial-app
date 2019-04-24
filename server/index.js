27856722f7b810741802a7edd12015da11d2a581
James Pham <jpham@Jamess-MBP.hsd1.ca.comcast.net>
2019-04-23 21:59:01 -0700

    added db to server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Pool } = require('pg');
 
 const app = express();
 app.use(bodyParser.json());
 
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+const msg = (success, status) => ({
+  success,
+  status
+});
+
 app.get('/', (req, res) => {
   res.send(JSON.stringify(process.env));
+});
+
+app.post('/auth/register', async (req, res) => {
+  if(!req.body || !req.body.username || !req.body.password) {
+    res.status(400).json(msg(false, 'Required { username: String, password: String }'));
+    return;
+  }
+  const result = await db.query(
+    'SELECT * FROM users WHERE username = $1', [req.body.username]
+    );
+  console.log(result);
+  res.status(200).json(msg(true, 'nothing'));
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
