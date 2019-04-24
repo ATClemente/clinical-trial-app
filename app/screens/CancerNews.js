585d17ad29a0c6ce2375c68426590c83520283f4
dianay90 <33560315+dianay90@users.noreply.github.com>
2019-04-23 22:19:39 -0700

    add cancer news js

---
+++ app/screens/CancerNews.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, Button, AsyncStorage } from 'react-native';
+import { ExpoConfigView } from '@expo/samples';
+
+export default class CancerNews extends React.Component {
+  static navigationOptions = {
+    title: 'Latest Cancer News',
+  };
+
+  _signOutAsync = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate('Auth');
+  };
+
+  render() {
+    /* Go ahead and delete ExpoConfigView and replace it with your
+     * content, we just wanted to give you a quick view of your config */
+    return (
+      <View>
+            <ExpoConfigView />
+
+
+            <Button title="Sign Out" onPress={this._signOutAsync} />
+
+
+
+
+
+      </View>
+    );
+  }
+}
