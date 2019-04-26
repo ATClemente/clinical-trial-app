ade88bfbbecd50b5679a8caa6f07ed335e6d212a
dianay90 <33560315+dianay90@users.noreply.github.com>
2019-04-25 21:26:02 -0700

    getting api stuff to show up on cancer news

--- app/screens/CancerNews.js
+++ app/screens/CancerNews.js
@@ -1,32 +1,119 @@
 import React from 'react';
-import { View, Button, AsyncStorage } from 'react-native';
+import { FlatList, ActivityIndicator, Text, WebView, View, Button, AsyncStorage} from 'react-native';
 import { ExpoConfigView } from '@expo/samples';
 
 export default class CancerNews extends React.Component {
-  static navigationOptions = {
-    title: 'Latest Cancer News',
-  };
+   //Cite: https://facebook.github.io/react-native/docs/network
 
-  _signOutAsync = async () => {
-    await AsyncStorage.clear();
-    this.props.navigation.navigate('Auth');
-  };
+    constructor(props) {
+        super(props);
+        this.state = { isLoading: true }
+    }
 
-  render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return (
-      <View>
-            <ExpoConfigView />
+    componentDidMount() {
+        return fetch('http://api.springernature.com/metadata/json?q=keyword:immunotherapy sort:date year:2019&api_key=1578185c036380f5d680e30d3e8d7349&p=50', { method: 'GET' })
+            .then((response) => response.json())
+            .then((responseJson) => {
+
+                this.setState({
+                    isLoading: false,
+                    dataSource: responseJson.records,
+                }, function () {
+
+                });
+
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
 
 
-            <Button title="Sign Out" onPress={this._signOutAsync} />
+    render() {
 
+        if (this.state.isLoading) {
+            return (
+                <View style={{ flex: 1, padding: 20 }}>
+                    <ActivityIndicator />
+                </View>
+            )
+        }
 
-
-
-
-      </View>
-    );
-  }
+        return (
+            <View style={{ flex: 1, paddingTop: 20 }}>
+                <FlatList
+                    data={this.state.dataSource}
+                    renderItem={({ item }) => <Text>{item.title}, {item.abstract}</Text>}
+                    keyExtractor={({ id }, index) => id}
+                />
+            </View>
+        );
+    }
 }
+
+
+    /*
+    //Cite: https://www.tutorialspoint.com/react_native/react_native_http.htm
+
+    componentDidMount = () => {
+
+        fetch('http://api.springernature.com/metadata/json?q=keyword:immunotherapy sort:date year:2019&api_key=1578185c036380f5d680e30d3e8d7349&p=50', { method: 'GET' })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+                this.setState({
+                    data: responseJson
+                })
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+    }
+    static navigationOptions = {
+        title: 'Latest Cancer News',
+    };
+
+    _signOutAsync = async () => {
+        await AsyncStorage.clear();
+        this.props.navigation.navigate('Auth');
+    };
+
+
+    render() {
+
+        return (
+
+
+
+            <View>
+
+                <Text> Find Latest Research on Cancer  </Text>
+                <Text>
+                    {this.state.data.body}
+                </Text>
+            </View>
+
+        )
+
+
+    }
+    /*
+    render() {
+
+        // const feed = require('./index.html');
+        return (
+            <WebView
+                //  source={{ feed }}
+                source={require('./index.html')}
+                originWhitelist={['*']}
+                style={{ flex: 1 }}
+            
+            />
+        );
+    }
+
+*/
+
+
+//export default CancerNews;
--- app/screens/SettingsScreen.js
+++ app/screens/SettingsScreen.js
@@ -4,7 +4,7 @@ import { ExpoConfigView } from '@expo/samples';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: 'Settings',
   };
 
   _signOutAsync = async () => {
@@ -12,14 +12,17 @@ export default class SettingsScreen extends React.Component {
     this.props.navigation.navigate('Auth');
   };
 
-  render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return (
-      <View>
-        <ExpoConfigView />
-        <Button title="Sign Out" onPress={this._signOutAsync} />
-      </View>
-    );
-  }
+
+    render() {
+        /* Go ahead and delete ExpoConfigView and replace it with your
+         * content, we just wanted to give you a quick view of your config */
+        return (
+            <View>
+                <ExpoConfigView />
+                <Button title="Sign Out" onPress={this._signOutAsync} />
+            </View>
+        );
+    }
+    
+  
 }
