9f8b866c58f371a19b6edce4c4e0c35f3acb3426
ATClemente <anthony.clemente13@gmail.com>
2019-05-08 20:13:20 -0400

    Example of getting data for one trial

--- app/screens/SavedTrialsScreen.js
+++ app/screens/SavedTrialsScreen.js
@@ -5,6 +5,8 @@ import {
   Text,
   View,
  } from 'react-native';
+ import ClinicalTrialAPIUtil from '../components/ClinicalTrialAPIUtil.js';
+import * as QueryConstants from '../constants/MainSearchQueryParams.js';
 
 export default class SavedTrialScreen extends React.Component {
   constructor(props) {
@@ -16,6 +18,7 @@ export default class SavedTrialScreen extends React.Component {
 
   render() {
     // console.log(this.state.trials);
+    this._getSingleTrialInfo(this.state.trials[0]);
     return (
       <View>
         <Text>Location {this.global.profile.location}</Text>
@@ -35,4 +38,24 @@ export default class SavedTrialScreen extends React.Component {
       <Text>{item.createdDate}</Text>
     </View>
   )
+
+  _getSingleTrialInfo = (trial) => {
+    var params = {};
+    params[QueryConstants.NCT_ID] = trial.trialId;
+    params[QueryConstants.INCLUDE_STR] = QueryConstants.INCLUDE_ARR;
+
+    ClinicalTrialAPIUtil.sendPostRequest(params)
+    .then((response) => {
+      if(response){
+        console.log("Returning trial with only these fields: ");
+        for(var key in response.trials[0]){
+          console.log(key);
+        }
+        //console.log(response);
+      }
+      else{
+        console.log("Error getting single trial data");
+      }
+    }); 
+  }
 }
