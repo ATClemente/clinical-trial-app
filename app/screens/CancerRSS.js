347a016935974eabddbc1068679b541cd95336a8
dianay90 <33560315+dianay90@users.noreply.github.com>
2019-05-08 20:11:22 -0700

    add rss formatting

--- app/screens/CancerRSS.js
+++ app/screens/CancerRSS.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { FlatList, Linking,AppRegistry, StyleSheet,ActivityIndicator, Text, WebView, View, Button, AsyncStorage} from 'react-native';
 import { ExpoConfigView } from '@expo/samples';
 var XMLParser = require('react-xml-parser');
-
+import { Card, CardItem, Left, Body, Container, Content } from 'native-base';
+import GradientButton from '../components/GradientButton';
+import Colors from '../constants/Colors';
 
 export default class CancerRSS extends React.Component {
     //Cite: https://facebook.github.io/react-native/docs/network
@@ -40,7 +42,11 @@ export default class CancerRSS extends React.Component {
 
     componentDidMount() {
       // const rssURL = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Ffeed.rssunify.com%2F5cbc0dbe0657c%2Frss.xml&api_key=aw46lnwivmgetk7aos9xwiv1yb8xur8ogewi0epn&order_by=pubDate&order_dir=desc&count=50'
-        const rssURL = 'https://api.rss2json.com/v1/api.json?rss_url=http://feeds.feedburner.com/ncinewsreleases&api_key=aw46lnwivmgetk7aos9xwiv1yb8xur8ogewi0epn&order_by=pubDate&order_dir=desc&count=50'
+       // const rssURL = 'https://api.rss2json.com/v1/api.json?rss_url=http://feeds.feedburner.com/ncinewsreleases&api_key=aw46lnwivmgetk7aos9xwiv1yb8xur8ogewi0epn&order_by=pubDate&order_dir=desc&count=50'
+        const rssURL = 'https://api.rss2json.com/v1/api.json?rss_url=https://feed.rssunify.com/5cd3956da2842/rss.xml&api_key=aw46lnwivmgetk7aos9xwiv1yb8xur8ogewi0epn&order_by=pubDate&order_dir=desc&count=70'
+
+        http://www.rssmix.com/u/8318797/rss.xml
+        
         /*
         fetch(rssUrl)
             .then(response => response.json())
@@ -96,6 +102,83 @@ export default class CancerRSS extends React.Component {
 
 
 */
+
+    render() {
+        // console.log(this.state.dataSource);
+        if (this.state.isLoading) {
+            return (
+                <View style={{ flex: 1, padding: 20 }}>
+                    <ActivityIndicator />
+                </View>
+            )
+        }
+
+        return (
+            <Container style={{ paddingHorizontal: 5, backgroundColor: '#ddd' }}>
+                <Content >
+
+                    <FlatList
+                        style={{ marginVertical: 4 }}
+                        data={this.state.dataSource}
+                        renderItem={this._renderItem}
+                        //keyExtractor={(item, index) => item.doi}
+                    />
+                </Content>
+            </Container>
+        );
+    }
+
+
+    _renderItem = ({ item }) => {
+        //<Text style={styles.body}>{item.title}, {item.publicationDate}, {item.abstract}</Text>}
+
+        return (
+            <Card>
+                <CardItem bordered style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
+
+
+
+
+
+                    <Text 
+                        onPress={() => Linking.openURL(item.link)}
+                        style={styles.articleTitle}
+
+                    >
+                        {item.title}
+             
+                    </Text>
+                    <Text style={{ color: '#999' }}>Publication Date: {item.pubDate}</Text>
+
+
+                </CardItem>
+                <CardItem>
+                    <Body>
+                        <Text>
+                            Description:{item.description.replace('<p>', '').replace('</p>', '')} 
+                        </Text>
+                    </Body>
+                </CardItem>
+                <CardItem style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                    <View style={{ width: '100%', marginBottom: 5 }}>
+                        <GradientButton
+                            colors={[Colors.blueOne, Colors.blueTwo]}
+                            handleClick={() => Linking.openURL(item.link)}
+                            loading={false}
+                            disabled={false}
+                            text='View Article'
+                            padding={10}
+                        />
+                    </View>
+                </CardItem>
+            </Card>
+        )
+    }
+}
+
+
+
+    /*
     render() {
 
 
@@ -109,7 +192,6 @@ export default class CancerRSS extends React.Component {
         return (
 
             <View style={{ flex: 1, paddingTop: 1, justifyContent: 'space-between' }}>
-                {/* <Text style={styles.title}> Latest Cancer Research </Text> */}
 
                 <FlatList
                     data={this.state.dataSource}
@@ -127,29 +209,14 @@ export default class CancerRSS extends React.Component {
                             </Text>
 
 
-                            <Text style>Description:{item.description} </Text>
+                            <Text style>Description:{item.description.replace('<p>', '').replace('</p>', '')  } </Text>
                             <Text style>Link:{item.link} </Text> 
 
       
 
-                            {/*
-                            <Text style={styles.pubDate} >Publication Date:{item.publicationDate} </Text>
+                            
 
-                            <Text style={styles.articleTitle}
-                                onPress={() => Linking.openURL(item.url[0].value)}>
-                                {item.title}
-                            </Text>
-
-
-                            <Text style={styles.abstract}> {item.abstract} </Text>
-
-                           
-                            <Text style={styles.doi}
-                                onPress={() => Linking.openURL(item.url[0].value)}>
-                                {item.doi}
-                            </Text>
-
-                            */}
+/*
                             <View
                                 style={{
                                     borderBottomColor: 'grey',
@@ -166,25 +233,67 @@ export default class CancerRSS extends React.Component {
                 />
             </View>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
+          
             );
 
     }
 }
+*/
+
+const styles = StyleSheet.create({
+    title: {
+        fontWeight: 'bold',
+        fontSize: 22,
+        alignItems: 'center',
+        textAlign: 'center',
+        fontFamily: 'Georgia'
+
+    },
+
+    articleTitle: {
+        fontWeight: 'bold',
+        color: '#3961a3',
+        fontSize: 17,
+        marginBottom: 3
+    },
+
+    doi: {
+        color: 'blue',
+        marginLeft: 15,
+        marginRight: 15
+    },
+
+    pubDate: {
+        fontWeight: 'bold',
+        fontSize: 17,
+        padding: 5,
+        // alignItems: 'center',
+        // textAlign: 'center',
+        marginLeft: 15,
+        marginRight: 15
+
+
+    },
+
+    abstract: {
+
+        fontSize: 15,
+        color: '#4d4d4d',
+        // padding:10,
+        // alignItems: 'center',
+        // textAlign: 'center',
+        marginLeft: 15,
+        marginRight: 15
+
+    },
+
+    bigBlue: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30,
+    },
+    body: {
+        color: 'black',
+        padding: 10,
+    },
+});
