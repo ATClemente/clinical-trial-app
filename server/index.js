a1825888c324fe0711e90fa32d9ae7da14855615
James Pham <jpham@Jamess-MBP.hsd1.ca.comcast.net>
2019-05-23 17:05:05 -0700

    resolved merge conflict

--- server/index.js
+++ server/index.js
@@ -4,15 +4,9 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { Pool } = require('pg');
-<<<<<<< HEAD
 const { parse } = require('node-html-parser');
 const request = require('request');
 
-=======
-const request = require('request');
-//import { parse } from 'node-html-parser';
-const {parse} = require('node-html-parser')
->>>>>>> 51c7d7be921f6fd3d36a3191f4e74e175c3aa673
 const app = express();
 app.use(bodyParser.json());
 const saltRounds = 10;
@@ -329,4 +323,3 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
-
