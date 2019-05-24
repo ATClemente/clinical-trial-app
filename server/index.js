9f78e3f39e7eebcab74e186fde12ce628a5f4e35
James Pham <jpham@Jamess-MBP.hsd1.ca.comcast.net>
2019-05-23 17:01:02 -0700

    added API for drugs

--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { Pool } = require('pg');
+const { parse } = require('node-html-parser');
+const request = require('request');
 
 const app = express();
 app.use(bodyParser.json());
-const saltRounds = 5;
+const saltRounds = 10;
 const SECRET = 'fdSj3sdAd59daSqLDasieQ9osM';
 
 const db = new Pool({
@@ -285,6 +287,35 @@ app.delete('/user/trials/:tid', async (req, res) => {
     console.log(e);
     res.status(500).json(serverError);
   }
+});
+
+app.get('/drugs', async (req, res) => {
+  request(
+    'https://www.centerwatch.com/drug-information/fda-approved-drugs/therapeutic-area/12/oncology',
+    (error, response, body) => {
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      const root = parse(body);
+      const contents = root.querySelector('#ctl00_BodyContent_AreaDetails');
+      const drugs = contents.childNodes.filter(e => e.tagName === 'p');
+      const result = drugs.map((e, index) => {
+        const text = e.childNodes[1].rawText.split(';');
+        const link = e.childNodes[0].rawAttrs
+          .split(' ')[1]
+          .split('=')[1]
+          .slice(1, -1);
+        return {
+          index,
+          link,
+          id: link.split('/')[4],
+          name: e.childNodes[0].childNodes[0].rawText.trim(),
+          manufacturer: text[1].trim(),
+          description: text[2].split(', Approved ')[0].trim(),
+          approval_date: text[2].split(', Approved ')[1]
+        };
+      });
+      res.status(200).json(result);
+    }
+  );
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
