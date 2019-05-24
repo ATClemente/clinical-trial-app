46183dad3cf8c3d814e8d9c6f252cfb021fc9a0d
dianay90 <33560315+dianay90@users.noreply.github.com>
2019-05-23 19:34:46 -0700

    adding the parse

--- app/screens/FDA_Drugs.js
+++ app/screens/FDA_Drugs.js
@@ -1,38 +1,66 @@
 import React from 'react';
-import { Image, ScrollView, StyleSheet, Text, BrowserView, WebView, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, BrowserView, WebView, View, FlatList } from 'react-native';
 import DOMParser from 'react-native-html-parser';
+import { Card, CardItem, Left, Body, Container, Content } from 'native-base';
+
 
 export default class FDA_Drugs extends React.Component {
     constructor(props) {
         super(props);
         this.state = { isLoading: true }
 
+    }
 
-    fetch('https://www.centerwatch.com/drug-information/fda-approved-drugs/therapeutic-area/12/oncology').then((response) => {
-   const html = response.text();    
-   const parser = new DOMParser.DOMParser();
-   const parsed = parser.parseFromString(html, 'text/html');
-   const test = parsed.getElementsById('ctl00_BodyContent_AreaDetails');
-    console.log(test)
-});
+    componentDidMount() {
+        return fetch('http://clinical-trial-app.herokuapp.com/drugs', { method: 'GET' })
+            .then((response) => response.json())
+            .then((responseJson) => {
+
+                this.setState({
+                    isLoading: false,
+                    dataSource: responseJson,
+                    
+                }, function () {
+
+                });
+
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
     render() {
 
-
-
-        
         return (
 
-        <View>
-            <Text> hi </Text>
-            </View>
+            <FlatList
+data={this.state.dataSource}
+renderItem={this._renderItem}
+/>
+      
 
 
         );
     }
+
+
+
+_renderItem = ({ item, index }) => {
+
+return(
+
+
+    <Text>
+
+        {item.name}
+    </Text>
+);
+
+
 }
 
+}
 styles = StyleSheet.create({
     abstract: {
         fontSize: 17,
