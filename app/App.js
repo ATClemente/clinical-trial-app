8496987ac8ecc5f92947f057d6cf43a349647e6d
James Pham <jpham@Jamess-MBP.hsd1.ca.comcast.net>
2019-06-04 03:42:57 -0700

    preload onboarding images

--- app/App.js
+++ app/App.js
@@ -49,6 +49,9 @@ export default class App extends React.Component {
       Asset.loadAsync([
         require('./assets/images/robot-dev.png'),
         require('./assets/images/robot-prod.png'),
+        require('./assets/images/01_search.png'),
+        require('./assets/images/02_view.png'),
+        require('./assets/images/03_news.png')
       ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
